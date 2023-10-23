a92530b3388d2f4b6008705632fb6d5716485f58
Alfredo Tous <alfredotous2304@gmail.com>
2023-10-23 14:49:59 -0500

    Despliegue 2do menu

--- menu.py
+++ menu.py
@@ -9,9 +9,22 @@ while i==0:
     print("3. Eliminar productos")
     print("4. Salir")
     opcion = int(input())
+    if opcion==1:
+        while True:
+            print("Ha entrado al Menu, haga su pedido")
+            print("1. Hamburguesa   ==> 20000$")
+            print("2. Papas fritas  ==>  5000$")
+            print("3. CocaCola Zero ==>  4000$")
+            print("4. Nuggets combo ==> 22000$")
+            print("5. Helado grande ==>  7000$")
+            print("6. Volver al menu anterior")
+            opcion2 = int(input())
+            if opcion2==6:
+                break
     if opcion == 4:
         print("Hasta luego")
         i=1
+
         
    
 
