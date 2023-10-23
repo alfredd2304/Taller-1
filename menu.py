f378b3994a4d88be2380e592de03985c2e561b50
Alfredo Tous <alfredotous2304@gmail.com>
2023-10-23 15:42:52 -0500

    Funcionalidad 2do Menu

--- menu.py
+++ menu.py
@@ -1,17 +1,20 @@
 print("Bienvenido al Restaurante, que deseas hacer?")
+print("(Digite numero)")
 
 compra = []
-
+costo_total = 0
 i=0
 while i==0:
     print("1. Ver Menu")
     print("2. Resumen del Pedido")
     print("3. Eliminar productos")
-    print("4. Salir")
+    print("4. Pagar Pedido")
+    print("5. Salir")
     opcion = int(input())
     if opcion==1:
+        print(" ")
+        print("Ha entrado al Menu, haga su pedido")
         while True:
-            print("Ha entrado al Menu, haga su pedido")
             print("1. Hamburguesa   ==> 20000$")
             print("2. Papas fritas  ==>  5000$")
             print("3. CocaCola Zero ==>  4000$")
@@ -19,9 +22,43 @@ while i==0:
             print("5. Helado grande ==>  7000$")
             print("6. Volver al menu anterior")
             opcion2 = int(input())
+            if opcion2 == 1:
+                compra.append("Hamburguesa x1 ==>20000$")
+                costo_total = costo_total+20000
+                print(" ")
+                print("Anotado, algo mas?")
+                print(" ")
+            if opcion2 ==2:
+                compra.append("Papas Fritas x1 ==>5000$")
+                costo_total = costo_total+5000
+                print(" ")
+                print("Anotado, algo mas?")
+                print(" ")
+            if opcion2==3:
+                compra.append("CocaCola Zero x1 ==>4000$")
+                costo_total = costo_total+4000
+                print(" ")
+                print("Anotado, algo mas?")
+                print(" ")
+            if opcion2==4:
+                compra.append("Nuggets combo x1 ==>22000$")
+                costo_total = costo_total+22000
+                print(" ")
+                print("Anotado, algo mas?")
+                print(" ")
+            if opcion2==5:
+                compra.append("Helado grande x1 ==>7000$")
+                costo_total = costo_total+7000
+                print(" ")
+                print("Anotado, algo mas?")
+                print(" ")
             if opcion2==6:
+                print(" ")
                 break
-    if opcion == 4:
+            else:
+                print("Digite un numero del 1 al 6")
+
+    if opcion == 5:
         print("Hasta luego")
         i=1
 
