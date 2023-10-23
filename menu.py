74adc262e1360369caec2c898e172d145abe7356
Alfredo Tous <alfredotous2304@gmail.com>
2023-10-23 14:32:08 -0500

    Primera interfaz

--- menu.py
+++ menu.py
@@ -1,3 +1,18 @@
 print("Bienvenido al Restaurante, que deseas hacer?")
 
 compra = []
+
+i=0
+while i==0:
+    print("1. Ver Menu")
+    print("2. Resumen del Pedido")
+    print("3. Eliminar productos")
+    print("4. Salir")
+    opcion = int(input())
+    if opcion == 4:
+        print("Hasta luego")
+        i=1
+        
+   
+
+
